7f7495be8c406b9bb4e791f4792bd0ac88292527
unknown <fog009922[@gmail.com](mailto:you@example.com)>
2026-01-09 11:55:40 +0900

    버튼 만들기 성공

--- number.py
+++ number.py
@@ -8,15 +8,21 @@ button = [
     "1", "2", "3",
     "4", "5", "6",
     "7", "8", "9",
-    "Del", "0", "결정", "취소"
+    "Del", "0", "결정"
 ]
 
+ent = tk.Entry(root)
+ent.grid(row=0, column=0, columnspan=4)
 
 for index, i in enumerate(button):
     r = index//3+1
     c = index%3
 
-    number_btn = bt.button(i, 10, 5, r, c)
-    number_btn.number_btn_maker(root)
-    
+    number_btn = bt.button(root, i, 3, 1, r, c)
+    number_btn.number_btn_maker()
+
+
+cancel_btn = bt.button(root, "취소", 3, 1, 4, 3)
+cancel_btn.number_btn_maker()
+
 root.mainloop()
