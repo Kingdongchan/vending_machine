0e7e332848fbc8cb771f422877b2565b0c233741
unknown <fog009922[@gmail.com](mailto:you@example.com)>
2026-01-09 14:26:19 +0900

    id붙이기

--- product.py
+++ product.py
@@ -20,20 +20,25 @@ bt_frm.pack(side="top", fill="both", expand=True)
 #상단 상품을 진열하는 곳 만들기
 
 menu = [
-    "코카콜라", "갈아만든 배", "사이다", "미에로화이바",
-    "초코송이", "에너지바", "다이제 씬", "빠다코코낫",
-    "신라면", "참꺠라면", "불닭볶음면", "육개장사발면",
-    "오레오", "화이트하임", "통크", "초코쿠키"
+    {"id":1, "상품":"코카콜라"}, {"id":2, "상품":"갈아만든 배"},{"id":3, "상품":"사이다"}, {"id":4, "상품":"미에로화이바"},
+    {"id":5, "상품":"초코송이"}, {"id":6, "상품":"에너지바"} , {"id":7, "상품":"다이제 씬"}, {"id":8, "상품":"빠다코코낫"},
+    {"id":9, "상품":"신라면"}, {"id":10, "상품":"참꺠라면"}, {"id":11, "상품":"불닭볶음면"}, {"id":12, "상품":"육개장사발면"},
+    {"id":13, "상품":"오레오"}, {"id":14, "상품":"화이트하임"}, {"id":15, "상품":"통크"}, {"id":16, "상품":"초코쿠키"}
 ]
 
 # 메뉴판 16개 생성
 for i in range(len(menu)):
-    prd = menu[i]
+    prd = menu[i]["상품"]
 
     r = i//4 
     c = i%4
-    
-    menu_frm = frm.frame(top_frm, 30, 30, r, c, True, "gray")
+
+    #모든 프레임에 가중치 주기(pack ->)
+    for j in range(4):
+        top_frm.grid_columnconfigure(j, weight=1)
+        top_frm.grid_rowconfigure(j, weight=1)
+
+    menu_frm = frm.frame(top_frm, 30, 30, r, c, True, "gray", prd)
     menu_frm.frm_maekr()
 
     
