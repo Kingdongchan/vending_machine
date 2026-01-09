282c60f7465db75d403f2337384354986d0ffba7
unknown <fog009922[@gmail.com](mailto:you@example.com)>
2026-01-09 15:28:39 +0900

    메인 창 완성

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import tkinter as tk
 
 import number as nmb
+import product as prd
 
 root = tk.Tk()
 root.title("자판기")
@@ -10,6 +11,9 @@ root.geometry("800x600")
 #메인프레임 -> 사이드프레임보다 커야하고 상품이 16가지가 들어가야함
 main_frm = tk.Frame(root, width=550 ,bg="lightgray")
 main_frm.pack(side="left", fill="y", expand = True)
+
+#메인프레임에 상품이랑 배출고 배치하기
+prd.main_frame_product(main_frm)
 
 #사이드 프레임(프레임 안에 입력칸, 결제칸, 번호칸, 현금을 누를 수 있는 칸이 필요함)
 #사이드 프레임 
--- product.py
+++ product.py
@@ -1,61 +1,65 @@
 import tkinter as tk
 import cls.frame as frm
 
+def main_frame_product (main_frm):
 
-root =tk.Tk()
-root.geometry("550x600")
-
-#프레임이 2개로 나눠져야함
-#상단 - 상품을 진열하는 곳
-#하단 - 배출구 -> 오른쪽 하단에 있어야함
-
-#상품을 진열할 곳
-top_frm = tk.Frame(root, width= 550, height= 600, bg="lightgray")
-top_frm.pack(side="top", fill="both", expand=True)
+    #프레임이 2개로 나눠져야함
+    #상단 - 상품을 진열하는 곳
+    #하단 - 배출구 -> 오른쪽 하단에 있어야함
+    #상품을 진열할 곳
+    top_frm = tk.Frame(main_frm, width= 600, height=500, bg="lightgray")
+    top_frm.grid(row=0, column=0, stick="nsew" )
 
 
-#상단 상품을 진열하는 곳 만들기
-menu = [
-    {"id":1, "상품":"코카콜라", "price": 1400}, 
-    {"id":2, "상품":"갈아만든 배", "price": 1300},
-    {"id":3, "상품":"사이다", "price": 1200}, 
-    {"id":4, "상품":"미에로화이바", "price":900},
-    {"id":5, "상품":"초코송이", "price": 1300}, 
-    {"id":6, "상품":"에너지바", "price": 1300},
-    {"id":7, "상품":"다이제 씬", "price": 1500}, 
-    {"id":8, "상품":"빠다코코낫", "price":1600},
-    {"id":9, "상품":"신라면", "price": 1300}, 
-    {"id":10, "상품":"참깨라면", "price": 1300}, 
-    {"id":11, "상품":"불닭볶음면", "price": 1500}, 
-    {"id":12, "상품":"육개장사발면", "price": 1300},
-    {"id":13, "상품":"오레오", "price": 1600}, 
-    {"id":14, "상품":"화이트하임", "price": 1500}, 
-    {"id":15, "상품":"통크", "price": 1300}, 
-    {"id":16, "상품":"초코쿠키", "price": 1300}
-]
+    main_frm.grid_rowconfigure(0, weight=9)
+    main_frm.grid_rowconfigure(1, weight=1)
+    main_frm.grid_columnconfigure(0, weight=1)
 
-# 메뉴판 16개 생성
-for i in range(len(menu)):
-    prd = menu[i]["상품"]
 
-    r = i//4 
-    c = i%4
+    #상단 상품을 진열하는 곳 만들기
+    menu = [
+        {"id":1, "상품":"코카콜라", "price": 1400}, 
+        {"id":2, "상품":"갈아만든 배", "price": 1300},
+        {"id":3, "상품":"사이다", "price": 1200}, 
+        {"id":4, "상품":"미에로화이바", "price":900},
+        {"id":5, "상품":"초코송이", "price": 1300}, 
+        {"id":6, "상품":"에너지바", "price": 1300},
+        {"id":7, "상품":"다이제 씬", "price": 1500}, 
+        {"id":8, "상품":"빠다코코낫", "price":1600},
+        {"id":9, "상품":"신라면", "price": 1300}, 
+        {"id":10, "상품":"참깨라면", "price": 1300}, 
+        {"id":11, "상품":"불닭볶음면", "price": 1500}, 
+        {"id":12, "상품":"육개장사발면", "price": 1300},
+        {"id":13, "상품":"오레오", "price": 1600}, 
+        {"id":14, "상품":"화이트하임", "price": 1500}, 
+        {"id":15, "상품":"통크", "price": 1300}, 
+        {"id":16, "상품":"초코쿠키", "price": 1300}
+    ]
 
-    #모든 프레임에 가중치 주기(pack ->)
-    for j in range(4):
-        top_frm.grid_columnconfigure(j, weight=1)
-        top_frm.grid_rowconfigure(j, weight=1)
+    # 메뉴판 16개 생성
+    for i in range(len(menu)):
+        prd = menu[i]["상품"]
+        prd_number = menu[i]["id"]
 
-    # 프레임마다 상품 이름 넣기
-    menu_frm = frm.frame(top_frm, 30, 30, r, c, True, "gray", prd)
-    menu_frm.frm_maekr()
+        r = i//4 
+        c = i%4
 
-#배출구를 만들 곳
-bt_frm = tk.Frame(root, width= 550, height=100, bg="red")
-bt_frm.pack(side="top", fill="both", expand=True)
+        #모든 프레임에 가중치 주기(pack ->)
+        for j in range(4):
+            top_frm.grid_columnconfigure(j, weight=1)
+            top_frm.grid_rowconfigure(j, weight=1)
 
-#배출구
-out_prd = frm.frame(bt_frm, 100, 100 , 0, 0, True, "white", None)
-out_prd.frm_bt_maker()
+        # 프레임마다 상품 이름 넣기
+        menu_frm = frm.frame(top_frm, 30, 30, r, c, True, "gray", f"{prd_number}. {prd}")
+        menu_frm.frm_maekr()
 
-root.mainloop()
+    #배출구를 만들 곳
+    bt_frm = tk.Frame(main_frm, bg="red")
+    bt_frm.grid(row=1, column=0, sticky="nsew")
+
+    bt_frm.grid_columnconfigure(0, weight=1)
+    bt_frm.grid_rowconfigure(0, weight=1)
+    # bt_frm.grid_rowconfigure(0, weight=1)
+    #배출구
+    out_prd = frm.frame(bt_frm, 100, 100 , 0, 0, False, "white", None)
+    out_prd.frm_bt_maker()
