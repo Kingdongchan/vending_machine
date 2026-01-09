a4030d54a501278bc5439ddfe7b0923a34105fa9
unknown <fog009922[@gmail.com](mailto:you@example.com)>
2026-01-09 16:48:39 +0900

    Entry 생성 완료

--- payment.py
+++ payment.py
@@ -1,5 +1,22 @@
 import tkinter as tk
 import coin 
 
-# 현금을 얼마나 넣었는지 확인할 수 있는 입력창 하나
-# 
+# 현금, 카드을 넣었는지 확인할 수 있는 입력창 하나
+# 평소에는 "돈을 넣어주십시오." 출력
+    #현금이라면
+        #금액을 확인하여 입력창에 더하기
+    #카드라면
+        #True 출력
+
+root = tk.Tk()
+#공간 생성
+buy_insert = tk.Entry(root, width=20, state="normal")
+buy_insert.pack(side="top")
+#"돈을 넣어주십시오." 넣기
+answer = "돈을 넣어주십시오."
+buy_insert.insert(0, answer)
+# 못만지게 disabled
+buy_insert.config(state="disabled")
+
+
+root.mainloop()
