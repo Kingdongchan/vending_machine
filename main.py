f8e89fa634fceeccaac88c106b67218ec6cd4542
unknown <fog009922[@gmail.com](mailto:you@example.com)>
2026-01-09 11:05:40 +0900

    사이드 칸 틀 완성

--- main.py
+++ main.py
@@ -11,6 +11,22 @@ main_frm.pack(side="left", fill="y", expand = True)
 
 #사이드 프레임(프레임 안에 입력칸, 결제칸, 번호칸, 현금을 누를 수 있는 칸이 필요함)
 #사이드 프레임 
-side_frm = tk.Frame(root, width=250, bg="gray")
+side_frm = tk.Frame(root, width=250, bg="white")
 side_frm.pack(side="left", fill="y", expand= True)
+
+#입력칸
+insert_frm = tk.Frame(side_frm, width=250, height=250, bg="white")
+insert_frm.pack(side="top", fill="x", expand=True)
+#결제칸
+payment_frm = tk.Frame(side_frm, width=250, height= 0, bg="white")
+payment_frm.pack(side="top", fill="x", expand=True)
+#번호칸
+number_frm = tk.Frame(side_frm, width=250, height =250, bg = "white")
+number_frm.pack(side= "top", fill="x", expand=True)
+
+#현금칸
+insert_button_frm = tk.Frame(side_frm, width=250, height= 30, bg = "white")
+insert_button_frm.pack(sid= "top", fill="x", expand=True)
+
+
 root.mainloop()
