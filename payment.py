1bc9363de3e12b005eb9d876b17ad126e78cb611
unknown <fog009922[@gmail.com](mailto:you@example.com)>
2026-01-09 16:59:11 +0900

    결제칸 입력창 생성

--- payment.py
+++ payment.py
@@ -1,6 +1,5 @@
 import tkinter as tk
-import coin 
-
+ 
 # 현금, 카드을 넣었는지 확인할 수 있는 입력창 하나
 # 평소에는 "돈을 넣어주십시오." 출력
     #현금이라면
@@ -8,15 +7,15 @@ import coin
     #카드라면
         #True 출력
 
-root = tk.Tk()
-#공간 생성
-buy_insert = tk.Entry(root, width=20, state="normal")
-buy_insert.pack(side="top")
-#"돈을 넣어주십시오." 넣기
-answer = "돈을 넣어주십시오."
-buy_insert.insert(0, answer)
-# 못만지게 disabled
-buy_insert.config(state="disabled")
+def payment (payment_frm):
+    global buy_insert
 
+    #공간 생성
+    buy_insert = tk.Entry(payment_frm, width=20, state="normal")
+    buy_insert.pack(side="top")
+    #"돈을 넣어주십시오." 넣기
+    answer = "돈을 넣어주십시오."
+    buy_insert.insert(0, answer)
+    # 못만지게 disabled
+    buy_insert.config(state="disabled")
 
-root.mainloop()
